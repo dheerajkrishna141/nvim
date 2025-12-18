91a305147fe6a1bf02580a0a52717d6f57699e5e
dheerajkrishna141 <dheerajkrishna141@gmail.com>
2025-12-18 00:38:59 -0500

    Updated conform.lua with correct name to use them in tsx and jsx files.

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -10,6 +10,8 @@ return {
             -- Conform will run the first available formatter
             javascript = { "prettierd", "prettier", stop_after_first = true },
             typescript = { "prettierd", "prettier", stop_after_first = true },
+            javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+            typescriptreact = { "prettierd", "prettier", stop_after_first = true },
             xml = { "xmlformatter" },
         },
         format_on_save = {
