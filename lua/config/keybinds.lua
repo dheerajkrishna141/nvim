c0de5f601cfbae97b529819a39311778b71b4614
dheerajkrishna141 <dheerajkrishna141@gmail.com>
2025-12-18 00:38:59 -0500

    Conform:
    - Added Conform formatter and formatted certain files to test the same.

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,2 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>cf", function()
+	require("conform").format({
+		lsp_format = "fallback",
+	})
+end, { desc = "Format current file" })
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,19 +1,23 @@
 return {
 
-    'nvim-treesitter/nvim-treesitter',
-    build = ":TSUpdate",
-    config = function()
-	local configs = require("nvim-treesitter.configs")
-	configs.setup({
-	    highlight = {
-		enable = true
-	    },
-	    indent = { enable = true },
-	    autotag = { enable = true },
-	    ensure_installed = {
-		"lua", "tsx", "typescript", "java", "dart"
-	    },
-	    auto_install = false
-	})
-    end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			highlight = {
+				enable = true,
+			},
+			indent = { enable = true },
+			autotag = { enable = true },
+			ensure_installed = {
+				"lua",
+				"tsx",
+				"typescript",
+				"java",
+				"dart",
+			},
+			auto_install = true,
+		})
+	end,
 }
