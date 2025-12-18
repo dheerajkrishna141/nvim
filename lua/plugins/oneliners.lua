f1e477fe3bc1737650bb3d52d10c68e3c7d03ba2
dheerajkrishna141 <dheerajkrishna141@gmail.com>
2025-12-18 00:38:59 -0500

    Added github copilot plugin.

--- lua/plugins/oneliners.lua
+++ lua/plugins/oneliners.lua
@@ -1,18 +1,20 @@
 return {
-    {
-	'tpope/vim-fugitive',
-    },
-     { -- Git plugin
-        'tpope/vim-fugitive',
-    },
-    { -- Show historical versions of the file locally
-        'mbbill/undotree',
-    },
-    { -- Show CSS Colors
-        'brenoprata10/nvim-highlight-colors',
-        config = function()
-            require('nvim-highlight-colors').setup({})
-        end
-    },
+	{
+		"tpope/vim-fugitive",
+	},
+	{ -- Git plugin
+		"tpope/vim-fugitive",
+	},
+	{ -- Show historical versions of the file locally
+		"mbbill/undotree",
+	},
+	{
+		"github/copilot.vim",
+	},
+	{ -- Show CSS Colors
+		"brenoprata10/nvim-highlight-colors",
+		config = function()
+			require("nvim-highlight-colors").setup({})
+		end,
+	},
 }
-
